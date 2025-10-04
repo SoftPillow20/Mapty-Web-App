32d563c110ae0f24591c924712cff7aa7c5903d8
SoftPillow20 <Nobila.Bernie19@gmail.com>
2025-10-04 19:14:52 +0800

    restructuring js files for MVC architecture: workout, running, and cycling set to models

---
+++ src/js/models/running.js
@@ -0,0 +1,16 @@
+import { Workout } from './workout.js';
+
+export class Running extends Workout {
+  type = 'running';
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+    this._setDescription();
+  }
+
+  calcPace() {
+    // min/km
+    this.pace = this.duration / this.distance;
+  }
+}
